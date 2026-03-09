5a92f536984dcb2d2d12e89ff0d561d62aedb1f8
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-03-09 17:04:54 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 125 ms, Memory: 29,904 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -9,8 +9,9 @@ class Solution
 	static int monthPrice; // 1달 이용권. 매달 1일부터 시작
 	static int threePrice; // 3달 이용권. 매달 1일부터 시작
 	static int yearPrice; // 1년 이용권. 1월 1일부터 시작
-	static int[] plan = new int[12]; // 1년 계획
+	static int[] plan = new int[13]; // 1년 계획
 	static int minPrice;
+	static int[] accumPrice = new int[13];
     
 	public static void main(String args[]) throws Exception
 	{
@@ -24,43 +25,28 @@ class Solution
 			threePrice = sc.nextInt();
 			yearPrice = sc.nextInt();
 
-			for (int i = 0; i < 12; i++) {
+			for (int i = 1; i <= 12; i++) {
 				plan[i] = sc.nextInt();
 			}
 
 			// 인풋 받음
 
 			minPrice = yearPrice;
-			priceCal(0, 0);
-
+			
+			//매달 가장 저렴한 가격 조회해서 저장
+			for(int i = 1;i<=12;i++) {
+				int nowPrice = Math.min(dayPrice*plan[i], monthPrice);
+				accumPrice[i]=accumPrice[i-1]+nowPrice;
+				
+				if(i>=3) {
+					if(threePrice<accumPrice[i]) {
+						accumPrice[i] = Math.min(accumPrice[i-3]+threePrice, accumPrice[i]);
+					}
+				}
+			}
+			minPrice=Math.min(yearPrice, accumPrice[12]);
 			System.out.println("#" + tc + " " + minPrice);
 
 		} // tc 끝
 	} // main 끝
-
-	static void priceCal(int month, int nowPrice) {
-
-		// 가지치기
-		if (nowPrice >= minPrice) {
-			return;
-		}
-
-		// 종료조건
-		if (month >= 12) {
-			minPrice = Math.min(nowPrice, minPrice);
-			return;
-		}
-
-		// 재귀
-
-		// 1일 이용권
-		priceCal(month + 1, nowPrice + dayPrice * plan[month]);
-
-		// 1달 이용권
-		priceCal(month + 1, nowPrice + monthPrice);
-
-		// 3달 이용권
-		priceCal(month + 3, nowPrice + threePrice);
-
-	}
 }
