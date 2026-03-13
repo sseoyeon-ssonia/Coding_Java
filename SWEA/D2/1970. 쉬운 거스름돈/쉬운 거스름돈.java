12afed2be328d7d80c602e90cd4a0581426fa795
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-03-13 13:52:57 +0900

    [D2] Title: 쉬운 거스름돈, Time: 103 ms, Memory: 27,648 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,48 +1,31 @@
-
-import java.util.Arrays;
 import java.util.Scanner;
-
-public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int[] units = {5000, 1000,500, 100, 50, 10, 5, 1};
-		
-		int t = sc.nextInt();
-		for(int tc = 1;tc<=t;tc++) {
-			int n = sc.nextInt()/10;
-			
-			int[] moneyDP = new int[n+1];
-			int[] path = new int[n+1];
-			
-			Arrays.fill(moneyDP, Integer.MAX_VALUE);
-			moneyDP[0]=0;
-			
-			
-			for(int i = 1;i<=n;i++) {
-				for(int j = 0;j<units.length;j++) {
-					if(i>=units[j]&&moneyDP[i-units[j]]+1<moneyDP[i]){
-						moneyDP[i]=moneyDP[i-units[j]]+1;
-						path[i]=j;
-					}
-				}
-			}
-			
-			int[] counts = new int[8];
-			int tempN = n;
-			while(tempN>0) {
-				int coinIdx = path[tempN];
-				counts[coinIdx]++;
-				tempN-=units[coinIdx];
-			}
-			
-			System.out.println("#"+tc);
-			for(int i = 0;i<8;i++) {
-				System.out.print(counts[i]+" ");
-			}
-			System.out.println();
-			
-		}
-		
-	}
+import java.io.FileInputStream;
+ 
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+ 
+        int test = sc.nextInt(); // 테스트케이스
+        for (int t = 1; t <= test; t++) {
+ 
+            int n = sc.nextInt(); // 거슬러줄 금액 n
+            int[] money = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
+            int[] ans = new int[8]; // 각 종류별로 필요 개수 담을 배열
+ 
+            for (int i = 0; i < 8; i++) {
+                ans[i] = n / money[i];
+                n %= money[i];
+            }
+ 
+            System.out.println("#" + t);
+            for (int i = 0; i < 8; i++) {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+ 
+        } // 테스트케이스 끝
+    }
 }
