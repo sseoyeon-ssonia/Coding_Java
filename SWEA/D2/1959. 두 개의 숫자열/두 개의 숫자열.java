09122e0776babbb4a9afed29d2ed1f206ea26585
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-03-30 17:30:40 +0900

    [D2] Title: 두 개의 숫자열, Time: 103 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -1,65 +1,89 @@
 
-import java.util.Scanner;
-import java.io.FileInputStream;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-
-		int test = sc.nextInt();
-		for (int t = 1; t <= test; t++) {
-
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int t = Integer.parseInt(st.nextToken());
+		for(int tc = 1;tc<=t;tc++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
 			int[] a;
 			int[] b;
-
-			// 항상 a가 b보다 같거나 짧도록 설정
-			if (n <= m) {
-				a = new int[n];
+			
+			if(n<=m) {
+				a= new int[n];
 				b = new int[m];
-
-				for (int i = 0; i < n; i++) {
-					a[i] = sc.nextInt();
+				
+				st = new StringTokenizer(br.readLine());
+				
+				for(int i= 0;i<n;i++) {
+					a[i] = Integer.parseInt(st.nextToken());
 				}
-				for (int i = 0; i < m; i++) {
-					b[i] = sc.nextInt();
+				
+				st = new StringTokenizer(br.readLine());
+				
+				for(int i= 0;i<m;i++) {
+					b[i] = Integer.parseInt(st.nextToken());
 				}
-			} else {
-				a = new int[m];
+			}else {
 				b = new int[n];
-
-				for (int i = 0; i < n; i++) {
-					b[i] = sc.nextInt();
+				a = new int[m];
+				
+				st = new StringTokenizer(br.readLine());
+				
+				for(int i= 0;i<n;i++) {
+					b[i] = Integer.parseInt(st.nextToken());
 				}
-				for (int i = 0; i < m; i++) {
-					a[i] = sc.nextInt();
+				
+				st = new StringTokenizer(br.readLine());
+				
+				for(int i= 0;i<m;i++) {
+					a[i] = Integer.parseInt(st.nextToken());
 				}
 			}
-
-			// 구할 최댓값
-			int maxMulti = Integer.MIN_VALUE;
-
-			for (int i = 0; i <= b.length - a.length; i++) {
-				// 해당 경우에서의 결과값
+			
+			//input 끝
+			
+			//항상 a<b
+			
+			int max = Integer.MIN_VALUE;
+			
+			for(int i = 0;i<=b.length-a.length;i++) { //b 출발점
 				int temp = 0;
-
-				for (int j = 0; j < a.length; j++) {
-					// 시작점 설정
-
-					temp += a[j] * b[i + j];
-
+				for(int j = i,k=0;j<i+a.length;j++,k++) {
+					temp+=a[k]*b[j];
 				}
-
-				maxMulti = Math.max(maxMulti, temp);
+				max=Math.max(temp, max);
 			}
-
-			System.out.printf("#%d %d%n", t, maxMulti);
-
-		} // 테스트케이스 끝
+			
+			System.out.println("#"+tc+" "+max);
+			
+			
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
