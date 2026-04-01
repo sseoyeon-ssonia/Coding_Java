eb7e1bafd0a2d4df9098eee54c2460702a89f3d9
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-01 17:14:34 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 53396 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+		Queue<Integer> q = new LinkedList<>();
+		for(int i = 1;i<=n;i++) {
+			q.add(i);
+		}
+		
+		int cnt = 0;
+		while(q.size()>1) {
+			if(cnt%2==0) {
+				q.poll();
+			}else {
+				int x = q.poll();
+				q.add(x);
+			}
+			cnt++;
+		}
+		
+		System.out.println(q.poll());
+	}
+}
