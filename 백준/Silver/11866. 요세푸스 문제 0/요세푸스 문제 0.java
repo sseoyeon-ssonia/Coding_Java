1f3b61329d25237d2f6c6d28b874694fc02fe716
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-02 15:53:17 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 152 ms, Memory: 28328 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,49 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		Queue<Integer> q = new LinkedList<>();
+
+//		Queue<Integer> tempQ = new LinkedList<>();
+
+		for (int i = 1; i <= n; i++) {
+			q.add(i);
+		}
+
+		StringBuilder ans = new StringBuilder();
+		ans.append("<");
+
+		while (!q.isEmpty()) {
+
+			for (int i = 0; i < k - 1; i++) {
+				int temp = q.poll();
+				q.add(temp);
+			}
+			int temp = q.poll();
+			ans.append(temp);
+			if (q.isEmpty())
+				break;
+			else {
+				ans.append(", ");
+			}
+
+		}
+		ans.append(">");
+		
+		
+		System.out.println(ans);
+	}
+}
