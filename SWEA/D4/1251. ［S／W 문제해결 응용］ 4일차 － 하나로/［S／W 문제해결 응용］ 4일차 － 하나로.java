4582ab1042a4203d6cd9f5d83f0612b18e41eaef
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-07 17:08:47 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 560 ms, Memory: 162,320 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -4,115 +4,109 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
- 
+
 public class Solution {
-    static int n;
-    static double ans;
-    static long[] x, y;
-    static double rate;
-    static int[] parent;
- 
-    static class Edge implements Comparable<Edge> {
-        int start;
-        int end;
-        double cost;
- 
-        public Edge(int start, int end) {
-            super();
-            this.start = start;
-            this.end = end;
-            this.cost = distance(start, end);
-        }
- 
-        @Override
-        public int compareTo(Edge o) {
-            return Double.compare(this.cost, o.cost);
-        }
- 
-    }
- 
-    static LinkedList<Edge> list;
- 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int T = Integer.parseInt(st.nextToken());
-        for (int tc = 1; tc <= T; tc++) {
- 
-            st = new StringTokenizer(br.readLine());
- 
-            n = Integer.parseInt(st.nextToken());
- 
-            x = new long[n]; // x좌표 배열
-            y = new long[n]; // y좌표 배열
- 
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                x[i] = Long.parseLong(st.nextToken());
-            }
- 
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                y[i] = Long.parseLong(st.nextToken());
-            }
- 
-            // x,y 좌표 입력
- 
-            st = new StringTokenizer(br.readLine());
-            rate = Double.parseDouble(st.nextToken());
- 
-            // 세율 입력
- 
-            // 입력 끝
- 
-            parent = new int[n]; // 부모 인덱스 저장
- 
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-            } // 초기화
- 
-            list = new LinkedList<>();
- 
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    list.add(new Edge(i, j));
-                }
-            }
- 
-            Collections.sort(list);
- 
-            ans = 0;
- 
-            for (int i = 0, pick = 0; i < list.size() && pick < n - 1; i++) {
-                Edge temp = list.get(i);
-                if (find(temp.start) != find(temp.end)) {
-                    union(temp.start,temp.end);
-                    ans += temp.cost;
-                    pick++;
-                }
-            }
- 
-            System.out.println("#" + tc + " " + Math.round(ans));
- 
-        }
- 
-    }
- 
-    static void union(int a, int b) {
-        parent[find(b)] = find(a);
-    }
- 
-    static int find(int a) {
-        if (a != parent[a])
-            parent[a] = find(parent[a]);
-        return parent[a];
-    }
- 
-    static double distance(int start, int end) {
-        long dist = (long) (Math.pow(Math.abs(x[start] - x[end]), 2) + Math.pow(Math.abs(y[start] - y[end]), 2));
- 
-        return rate * dist;
-    }
+	static int n;
+	static double ans;
+	static long[] x, y;
+	static double rate;
+	static boolean[] visited;
+
+	static class Edge implements Comparable<Edge> {
+		int end;
+		double cost;
+
+		public Edge(int end, double cost) {
+			super();
+			this.end = end;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Double.compare(this.cost, o.cost);
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(st.nextToken());
+		for (int tc = 1; tc <= T; tc++) {
+
+			st = new StringTokenizer(br.readLine());
+
+			n = Integer.parseInt(st.nextToken());
+
+			x = new long[n]; // x좌표 배열
+			y = new long[n]; // y좌표 배열
+			visited = new boolean[n];
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < n; i++) {
+				x[i] = Long.parseLong(st.nextToken());
+			}
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < n; i++) {
+				y[i] = Long.parseLong(st.nextToken());
+			}
+
+			// x,y 좌표 입력
+
+			st = new StringTokenizer(br.readLine());
+			rate = Double.parseDouble(st.nextToken());
+
+			// 세율 입력
+
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+			// 입력 끝
+
+			LinkedList<Edge>[] list = new LinkedList[n];
+
+			for (int i = 0; i < n; i++) {
+				list[i] = new LinkedList<>();
+			}
+
+			for (int i = 0; i < n - 1; i++) {
+				for (int j = i + 1; j < n; j++) {
+					list[i].add(new Edge(j, distance(i, j)));
+					list[j].add(new Edge(i, distance(j, i)));
+				}
+			}
+
+			ans = 0;
+
+			int pick = 0;
+
+			pq.add(new Edge(0, 0));
+
+			while (pick < n) {
+				Edge now = pq.poll();
+
+				if (visited[now.end])
+					continue;
+
+				visited[now.end] = true;
+				ans += now.cost;
+				pick++;
+				pq.addAll(list[now.end]);
+			}
+
+			System.out.println("#" + tc + " " + Math.round(ans));
+
+		}
+
+	}
+
+	static double distance(int start, int end) {
+		long dist = (long) (Math.pow(Math.abs(x[start] - x[end]), 2) + Math.pow(Math.abs(y[start] - y[end]), 2));
+
+		return rate * dist;
+	}
 }
