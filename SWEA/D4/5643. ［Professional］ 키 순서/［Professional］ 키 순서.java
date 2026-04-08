38f841def0e027c6b0b13eab42c57b7567f9f26c
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-09 00:37:50 +0900

    [D4] Title: [Professional] 키 순서, Time: 723 ms, Memory: 101,960 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,38 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+ 
         int T = Integer.parseInt(st.nextToken());
         for(int tc = 1;tc<=T;tc++){
-
+ 
             st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
-
+ 
             st = new StringTokenizer(br.readLine());
             int m = Integer.parseInt(st.nextToken());
-
+ 
             int[][] dist = new int[n][n];
             for(int i = 0;i<n;i++){
                 for(int j = 0;j<n;j++){
                     if(i!=j) dist[i][j]=Integer.MAX_VALUE;
                 }
             }//dist 초기화
-
+ 
             for(int i = 0;i<m;i++){
                 st = new StringTokenizer(br.readLine());
-
+ 
                 int start = Integer.parseInt(st.nextToken())-1;
                 int end = Integer.parseInt(st.nextToken())-1;
-
+ 
                 dist[start][end] = 1;
             }
-
-
+ 
+ 
             for(int x = 0;x<n;x++){
                 for(int from = 0;from<n;from++){
                     if(dist[from][x]==Integer.MAX_VALUE) continue;
@@ -43,7 +43,7 @@ public class Solution {
                     }
                 }
             }
-
+ 
             int ans = 0;
             for(int i = 0;i<n;i++){
                 int cnt = 0;
@@ -53,10 +53,10 @@ public class Solution {
                 }
                 if(cnt==n-1) ans++;
             }
-
+ 
             System.out.println("#"+tc+" "+ans);
-
+ 
         }//tc 끝
-
+ 
     }//main 끝
 }
