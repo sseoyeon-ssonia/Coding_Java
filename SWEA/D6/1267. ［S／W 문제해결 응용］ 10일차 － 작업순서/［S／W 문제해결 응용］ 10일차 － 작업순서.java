d8adadb532f8f2fdd67b3854d85033c31c7d9411
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-09 11:52:46 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 96 ms, Memory: 28,672 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -6,52 +6,52 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		for(int tc = 1;tc<=10;tc++) {
-			st = new StringTokenizer(br.readLine());
-			
-			int V = Integer.parseInt(st.nextToken());
-			int E = Integer.parseInt(st.nextToken());
-			
-			List<Integer>[] adjList = new ArrayList[V+1];
-			int[] inDegree = new int[V+1];
-			
-			for(int i = 1;i<V+1;i++) {
-				adjList[i]= new ArrayList<>();
-			}
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0;i<E;i++) {
-				int from = Integer.parseInt(st.nextToken());
-				int to = Integer.parseInt(st.nextToken());
-				
-				adjList[from].add(to);
-				inDegree[to]++;
-			}//입력 끝
-			
-			Queue<Integer> q = new ArrayDeque<>();
-			
-			for(int i = 1;i<V+1;i++) {
-				if(inDegree[i]==0) q.add(i);
-			}
-			
-			StringBuilder ans = new StringBuilder();
-			while(!q.isEmpty()) {
-				int temp = q.poll();
-				ans.append(temp+" ");
-				for(int now : adjList[temp]) {
-					inDegree[now]--;
-					if(inDegree[now]==0) q.add(now);
-					
-				}
-			}
-			
-			System.out.println("#"+tc+" "+ans);
-			
-		}
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        for(int tc = 1;tc<=10;tc++) {
+            st = new StringTokenizer(br.readLine());
+             
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+             
+            List<Integer>[] adjList = new ArrayList[V+1];
+            int[] inDegree = new int[V+1];
+             
+            for(int i = 1;i<V+1;i++) {
+                adjList[i]= new ArrayList<>();
+            }
+             
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0;i<E;i++) {
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                 
+                adjList[from].add(to);
+                inDegree[to]++;
+            }//입력 끝
+             
+            Queue<Integer> q = new ArrayDeque<>();
+             
+            for(int i = 1;i<V+1;i++) {
+                if(inDegree[i]==0) q.add(i);
+            }
+             
+            StringBuilder ans = new StringBuilder();
+            while(!q.isEmpty()) {
+                int temp = q.poll();
+                ans.append(temp+" ");
+                for(int now : adjList[temp]) {
+                    inDegree[now]--;
+                    if(inDegree[now]==0) q.add(now);
+                     
+                }
+            }
+             
+            System.out.println("#"+tc+" "+ans);
+             
+        }
+    }
 }
