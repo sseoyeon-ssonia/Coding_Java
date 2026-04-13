cbb4610ba6772d509bbfd4a5bcc84eae4a0673f8
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-14 08:52:25 +0900

    [D3] Title: 공과 잡초, Time: 82 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D3/14555. 공과 잡초/공과 잡초.java
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.java
@@ -1,49 +1,53 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-
-        int T = Integer.parseInt(st.nextToken());
-        for(int tc = 1;tc<=T;tc++){
-
-            Stack<Character> stack = new Stack<>();
-
-
-            int ans = 0;
-
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-
-            for(int i = 0;i<str.length();i++){
-                char now = str.charAt(i);
-
-                if(now =='.') continue;
-                else if(now=='(') stack.add(now);
-                else if(now == ')'){
-                    stack.pop();
-                    ans++;
-                }
-                else if(now == '|'){
-                    if(!stack.isEmpty()){
-                        if (stack.peek()=='('){
-                            stack.pop();
-                            ans++;
-                        }else if(stack.peek()=='|') continue;
-                    }else stack.add(now);
-
-                }
-
-
-            }
-
-            System.out.println("#"+tc+" "+ans);
-
-
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int T = Integer.parseInt(st.nextToken());
+		for(int tc = 1;tc<=T;tc++) {
+			st = new StringTokenizer(br.readLine());
+			
+			String str = st.nextToken();
+			Stack<Character> stack = new Stack<>();
+			int ans = 0;
+			
+			for(int i = 0;i<str.length();i++) {
+				char now = str.charAt(i);
+				
+				if(now=='.') continue;
+				else if(now == '(') {
+					stack.add(now);
+				}else if(now == '|') {
+					if(!stack.isEmpty()) {
+						if(stack.peek()=='(') {
+							ans++;
+							stack.pop();
+						}else {
+							stack.add(now);
+						}
+					}
+					else {
+						stack.add(now);
+					}
+				}else {
+					if(!stack.isEmpty()) {
+						ans++;
+						stack.pop();
+					}
+				}
+			}
+			
+			System.out.println("#"+tc+" "+ans);
+			
+		}//tc 끝
+				
+	}
 }
