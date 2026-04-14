aada0309e79e73cf8230dae82d6c9b5f97d93680
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-14 12:13:36 +0900

    [D2] Title: 파리퇴치3, Time: 161 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
@@ -1,75 +1,88 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
- 
- 
-class Solution
-{
-    static int[] dr1 = { -1, 1, 0, 0 };
-    static int[] dc1 = { 0, 0, -1, 1 };
-    static int[] dr2 = { -1, -1, 1, 1 };
-    static int[] dc2 = { -1, 1, -1, 1 };
- 
-    public static void main(String[] args) {
- 
-        Scanner sc = new Scanner(System.in);
- 
-        int test = sc.nextInt();
-        for (int t = 1; t <= test; t++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
- 
-            int[][] map = new int[n + m + m][n + m + m];
-            for (int i = m; i < n + m; i++) {
-                for (int j = m; j < n + m; j++) {
-                    map[i][j] = sc.nextInt();
-                }
-            }
- 
-            // 십자
-            int max = 0;
- 
-            for (int r = m; r < n + m; r++) {
-                for (int c = m; c < n + m; c++) {
-                    int sum = map[r][c];
- 
-                    for (int d = 0; d < 4; d++) {
-                        for (int k = 1; k < m; k++) {
- 
-                            int dr = r + dr1[d] * k;
-                            int dc = c + dc1[d] * k;
- 
-                            sum += map[dr][dc];
- 
-                        }
-                    }
-                    if (sum > max)
-                        max = sum;
- 
-                }
-            }
-            // 대각선
- 
-            for (int r = m; r < n + m; r++) {
-                for (int c = m; c < n + m; c++) {
-                    int sum = map[r][c];
-                    for (int d = 0; d < 4; d++) {
-                        for (int k = 1; k < m; k++) {
- 
-                            int dr = r + dr2[d] * k;
-                            int dc = c + dc2[d] * k;
- 
-                            sum += map[dr][dc];
- 
-                        }
-                    }
-                    if (sum > max)
-                        max = sum;
- 
-                }
-            }
- 
-            System.out.printf("#%d %d%n", t, max);
- 
-        }
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int[] dr1 = { -1, 1, 0, 0 };
+	static int[] dc1 = { 0, 0, -1, 1 };
+	static int[] dr2 = { -1, -1, 1, 1 };
+	static int[] dc2 = { -1, 1, -1, 1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(st.nextToken());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+
+			int[][] map = new int[n][n];
+
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			// input 끝
+
+			int maxFly = 0; // 최대 파리 수
+
+			// 십자
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+
+					int tempFly = 0;
+					for (int k = 0; k < 4; k++) {
+						int r = i;
+						int c = j;
+
+						for (int a = 1; a < m; a++) {
+							int nr = r + dr1[k] * a;
+							int nc = c + dc1[k] * a;
+
+							if (nr >= 0 && nc >= 0 && nr < n && nc < n) {
+								tempFly += map[nr][nc];
+							}
+						}
+					}
+					tempFly+=map[i][j];
+
+					maxFly = Math.max(maxFly, tempFly);
+				}
+			}
+
+			// 대각선
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+
+					int tempFly = 0;
+					for (int k = 0; k < 4; k++) {
+						int r = i;
+						int c = j;
+
+						for (int a = 1; a < m; a++) {
+							int nr = r + dr2[k] * a;
+							int nc = c + dc2[k] * a;
+
+							if (nr >= 0 && nc >= 0 && nr < n && nc < n) {
+								tempFly += map[nr][nc];
+							}
+						}
+					}
+					tempFly+=map[i][j];
+
+					maxFly = Math.max(maxFly, tempFly);
+				}
+			}
+
+			System.out.println("#" + tc + " " + maxFly);
+
+		} // tc 끝
+
+	}
 }
