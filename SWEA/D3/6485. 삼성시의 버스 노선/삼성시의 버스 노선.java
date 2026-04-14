834af6a5ff9c0d0e33a18d14141b33c9bda5a422
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-14 17:15:22 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 115 ms, Memory: 28,156 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
@@ -2,48 +2,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int T = Integer.parseInt(st.nextToken());
-		for(int tc = 1;tc<=T;tc++) {
-			
-			st = new StringTokenizer(br.readLine());
-			
-			int[] stops = new int[5001];
-			
-			int n = Integer.parseInt(st.nextToken());
-			
-			for(int i = 0;i<n;i++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				
-				for(int j = a;j<=b;j++) {
-					stops[j]++;
-				}
-				
-			}
-			
-			st = new StringTokenizer(br.readLine());
-			int p = Integer.parseInt(st.nextToken());
-			StringBuilder ans = new StringBuilder();
-			
-
-			for(int i = 0;i<p;i++) {
-				st = new StringTokenizer(br.readLine());
-				int now = Integer.parseInt(st.nextToken());
-				
-				ans.append(" ").append(stops[now]);
-				
-			}
-			
-			System.out.println("#"+tc+ans);
-			
-		}//tc 끝
-		
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+         
+        int T = Integer.parseInt(st.nextToken());
+        for(int tc = 1;tc<=T;tc++) {
+             
+            st = new StringTokenizer(br.readLine());
+             
+            int[] stops = new int[5001];
+             
+            int n = Integer.parseInt(st.nextToken());
+             
+            for(int i = 0;i<n;i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                 
+                for(int j = a;j<=b;j++) {
+                    stops[j]++;
+                }
+                 
+            }
+             
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken());
+            StringBuilder ans = new StringBuilder();
+             
+ 
+            for(int i = 0;i<p;i++) {
+                st = new StringTokenizer(br.readLine());
+                int now = Integer.parseInt(st.nextToken());
+                 
+                ans.append(" ").append(stops[now]);
+                 
+            }
+             
+            System.out.println("#"+tc+ans);
+             
+        }//tc 끝
+         
+    }
 }
