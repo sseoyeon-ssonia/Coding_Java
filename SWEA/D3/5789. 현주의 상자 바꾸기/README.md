1e5f9c84554f659c7e997a30fa9772add490541f
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-14 17:40:43 +0900

    [D3] Title: 현주의 상자 바꾸기, Time: 399 ms, Memory: 57,624 KB -BaekjoonHub

---
+++ SWEA/D3/5789. 현주의 상자 바꾸기/README.md
@@ -0,0 +1,15 @@
+# [D3] 현주의 상자 바꾸기 - 5789 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWYygN36Qn8DFAVm) 
+
+### 성능 요약
+
+메모리: 57,624 KB, 시간: 399 ms, 코드길이: 1,014 Bytes
+
+### 제출 일자
+
+2026-04-14 17:40
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(st.nextToken());
+		for (int tc = 1; tc <= T; tc++) {
+
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int q = Integer.parseInt(st.nextToken());
+
+			int[] box = new int[n + 1];
+
+			for (int i = 1; i <= q; i++) {
+				st = new StringTokenizer(br.readLine());
+				int L = Integer.parseInt(st.nextToken());
+				int R = Integer.parseInt(st.nextToken());
+
+				for (int j = L; j <= R; j++) {
+					box[j] = i;
+				}
+
+			}
+
+			StringBuilder ans = new StringBuilder();
+
+			for (int i = 1; i <= n; i++) {
+				ans.append(" ").append(box[i]);
+			}
+
+			System.out.println("#" + tc + ans);
+
+		} // tc 끝
+
+	}
+}
