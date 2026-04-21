85b010c10c3d82962d7f59130efd8d056c06ea76
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-21 16:25:29 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 110 ms, Memory: 31,104 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -6,103 +6,102 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int n,m,r,c,l, ans; //지도 세로크기 n 가로 m 맨홀 장소 세로 r 가로 c 탈출 후 시간 l 탈주범 위치 장소 개수 ans
+	static int n, m, r, c, l, ans; // 지도 크기n,m 맨홀 위치 r,c 소요시간 l
 	static int[][] map;
 	static boolean[][] visited;
-	
-	//우하좌상
-	static int[] dr = {0,1,0,-1};
-	static int[] dc = {1,0,-1,0};
 
-	static int[][] tunnel = { {},
-			{0,1,2,3},
-			{1,3},
-			{0,2},
-			{0,3},
-			{0,1},
-			{1,2},
-			{2,3}
-	};
-	
-	public static void main(String[] args) throws IOException{
+	// 상하좌우
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+
+	// 각 파이프별 이동 가능 방향
+	static int[][] dir = { {}, { 0, 1, 2, 3 }, { 0, 1 }, { 2, 3 }, { 0, 3 }, { 1, 3 }, { 1, 2 }, { 0, 2 } };
+
+	static int[] opposite = { 1, 0, 3, 2 };
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int T = Integer.parseInt(st.nextToken());
-		for(int tc = 1;tc<=T;tc++) {
-			
+		for (int tc = 1; tc <= T; tc++) {
+
 			st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			m = Integer.parseInt(st.nextToken());
 			r = Integer.parseInt(st.nextToken());
 			c = Integer.parseInt(st.nextToken());
 			l = Integer.parseInt(st.nextToken());
-			
+
 			map = new int[n][m];
-			visited = new boolean[n][m];
-			for(int i = 0;i<n;i++) {
+			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j = 0;j<m;j++) {
-					map[i][j]=Integer.parseInt(st.nextToken());
+				for (int j = 0; j < m; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
-			//input 끝
-			
+
+			// 입력 끝
+
+			visited = new boolean[n][m];
+
 			ans = 1;
 			bfs();
-			
-			System.out.println("#"+tc+" "+ans);
-			
-		}//tc 끝
-		
-	}//main 끝
-	
+			System.out.println("#" + tc + " " + ans);
+
+		} // tc 끝
+
+	}// main 끝
+
 	private static void bfs() {
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {r,c,1});
-		visited[r][c]=true;
-		
-		while(!q.isEmpty()) {
+
+		q.add(new int[] { r, c, 1 });
+
+		visited[r][c] = true;
+
+		while (!q.isEmpty()) {
 			int[] temp = q.poll();
 			int tempR = temp[0];
-			int tempL = temp[1];
+			int tempC = temp[1];
 			int tempTime = temp[2];
-			
-			if(tempTime==l) {
+
+			visited[tempR][tempC] = true;
+			if (tempTime == l)
 				continue;
-			}
-			
-			int type = map[tempR][tempL];
-			
-			for(int dir : tunnel[type]) {
-				int nr = tempR+dr[dir];
-				int nc = tempL + dc[dir];
-				
-				if(nr<0 || nr>=n || nc<0 || nc>=m || visited[nr][nc]) continue;
-				
-				if(map[nr][nc]==0) continue;
-				
-				int nextType = map[nr][nc];
-				int dir2 = (dir+2)%4;
-				
-				if(!canMove(nextType,dir2)) continue;
-				
+
+			int type = map[tempR][tempC];
+			for (int x : dir[type]) {
+				int nr = tempR + dr[x];
+				int nc = tempC + dc[x];
+
+				if (nr < 0 || nr >= n || nc < 0 || nc >= m || visited[nr][nc])
+					continue;
+
+				if (map[nr][nc] == 0)
+					continue;
+
+				// 연결되어있느지 확인
+				if (!check(x, nr, nc))
+					continue;
+
 				visited[nr][nc] = true;
 				ans++;
-				q.offer(new int[] {nr,nc,tempTime+1});
+				q.add(new int[] { nr, nc, tempTime + 1 });
+
 			}
-					
+
 		}
-		
-		
-	}//bfs 끝
-	
-	static boolean canMove(int type, int dir) {
-		for(int d : tunnel[type]) {
-			if(d==dir) return true;
+
+	}
+
+	private static boolean check(int type, int nr, int nc) {
+		int tempType = map[nr][nc];
+		int checkDir = opposite[type];
+		for (int x : dir[tempType]) {
+			if (checkDir == x)
+				return true;
 		}
 		return false;
 	}
-	
 }
