df8ee4bcbbe6057e4c2a33552aded5f937984328
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-04-22 12:01:28 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 105 ms, Memory: 28,928 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -8,76 +8,55 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		
-		for(int tc = 1;tc<=10;tc++) {
-			
+
+		for (int tc = 1; tc <= 10; tc++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int v = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
-			
-//			int[][] adjList = new int[v+1][v+1];
-			List<Integer>[] adjList = new ArrayList[v+1];
 
-			
-			for(int i = 1;i<=v;i++) {
-				adjList[i]=new ArrayList<>();
+			int[] inDegree = new int[v + 1];
+
+			List<Integer>[] list = new ArrayList[v + 1];
+
+			for (int i = 1; i <= v; i++) {
+				list[i] = new ArrayList<>();
 			}
-			
-			int[] inDegree = new int[v+1];
-			
+
 			st = new StringTokenizer(br.readLine());
-			for(int i = 0;i<e;i++) {
-				
+			for (int i = 0; i < e; i++) {
 				int start = Integer.parseInt(st.nextToken());
 				int end = Integer.parseInt(st.nextToken());
-				
-				adjList[start].add(end);
+
+				list[start].add(end);
 				inDegree[end]++;
-				
 			}
-			
-			//입력 끝
-			
-			StringBuilder ans = new StringBuilder();
-			
+
 			Queue<Integer> q = new ArrayDeque<>();
-			
-			for(int i = 1;i<=v;i++) {
-				if(inDegree[i]==0) {
+
+			for (int i = 1; i <= v; i++) {
+				if (inDegree[i] == 0)
 					q.add(i);
-				}
 			}
-			
-			while(!q.isEmpty()) {
+
+			StringBuilder ans = new StringBuilder();
+
+			while (!q.isEmpty()) {
 				int temp = q.poll();
 				ans.append(" ").append(temp);
-				
-				for(int now : adjList[temp]) {
-					
+
+				for (int now : list[temp]) {
 					inDegree[now]--;
-					if(inDegree[now]==0) {
+					if (inDegree[now] == 0)
 						q.add(now);
-					}
 				}
-				
 			}
-			
-			System.out.println("#"+tc+ans);
-			
-			
-		}//tc 끝
-		
-	}//main 끝
-}	
 
+			System.out.println("#" + tc + ans);
 
+		}
 
-
-
-
-
+	}
+}
