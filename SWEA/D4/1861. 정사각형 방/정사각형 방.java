58746f0d0b3c4f6509463812241466cc16b27a92
sseoyeon-ssonia <sseoyeon.ssonia@gmail.com>
2026-05-11 12:35:31 +0900

    [D4] Title: 정사각형 방, Time: 534 ms, Memory: 39,456 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -4,73 +4,73 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int bestStart, bestCnt; // 결과
-	static int[][] map;
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
+    static int n, maxCnt, startRoom; //크기 n, 최대 몇 개 방 이동, 출발 방 번호
+    static int[][] map;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
 
-		int T = Integer.parseInt(st.nextToken());
-		for (int tc = 1; tc <= T; tc++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			map = new int[n][n];
+        int t = Integer.parseInt(st.nextToken());
 
-			for (int i = 0; i < n; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
+        for(int tc = 1;tc<=t;tc++){
 
-			// 입력 끝
-			bestStart = 1;
-			bestCnt = 1;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        map=new int[n][n];
 
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					int r = i;
-					int c = j;
+        maxCnt=0;
+        startRoom=-1;
 
-					int nowResult = map[i][j];
-					int nowCnt = 1;
+        for(int i = 0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0;j<n;j++){
+                map[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
 
-					while (true) {
-						boolean flag = false;
-						for (int d = 0; d < 4; d++) {
-							int nr = r + dr[d];
-							int nc = c + dc[d];
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){//시작점
+                int nowCnt = 1;
+                int r = i;
+                int c = j;
 
-							if (nr >= 0 && nr < n && nc >= 0 && nc < n && map[nr][nc] == map[r][c] + 1) {
-								nowCnt++;
-								r = nr;
-								c = nc;
-								flag = true;
-								break;
-							}
+                while(true){
+                    boolean flag=false;
+                    for(int d = 0;d<4;d++){
+                        int nr = r+dr[d];
+                        int nc = c+dc[d];
 
-						}
-						if (!flag)
-							break;
-					}
+                        if(nr<0 || nr>=n || nc<0 || nc>=n || map[nr][nc]!=map[r][c]+1){
+                            continue;
+                        }
 
-					if (nowCnt > bestCnt) {
-						bestCnt = nowCnt;
-						bestStart = map[i][j];
-					} else if (nowCnt == bestCnt) {
-						bestStart = Math.min(bestStart, map[i][j]);
-					}
+                        flag=true;
+                        nowCnt++;
+                        r=nr;
+                        c=nc;
+                        break;
 
-				}
-			}
 
-			System.out.println("#" + tc + " " + bestStart + " " + bestCnt);
-		} // tc 끝
+                    }
+                    if(!flag) break;
 
-	}// main 끝
+                }
+                if(nowCnt>maxCnt){
+                    maxCnt=nowCnt;
+                    startRoom=map[i][j];
+                }else if(nowCnt==maxCnt){
+                    startRoom=Math.min(startRoom,map[i][j]);
+                }
+            }
+        }
+
+        System.out.println("#"+tc+" "+startRoom+" "+maxCnt);
+
+        }//tc 끝
+    }//main 끝
 
 }
