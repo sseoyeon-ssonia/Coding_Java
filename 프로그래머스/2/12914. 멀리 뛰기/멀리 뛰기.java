bd3c3fb2e08e15300dfa41f05b4f0cfb773b5779
Seoyeon Kim <sseoyeon.ssonia@gmail.com>
2026-06-12 09:48:13 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public long solution(int n) {
+        long answer = 0;
+
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        //dp
+
+        long[] dp = new long[n+1];
+
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i = 3;i<=n;i++){
+            dp[i]=(dp[i-1]+dp[i-2])%1234567;
+        }
+        
+        answer=dp[n];
+
+        return answer;
+    }
+
+}
